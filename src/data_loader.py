d6f0139482fa35fbad36f084e9fb6530de1e9a2c
joshuasevenc617 <joshuasevenc@gmail.com>
2025-02-19 22:41:57 +1100

    build data loader to download data from yahoo finance
    Example data is from apple stock
    time frame: 2020-01-01 to 2021-01-01

---
+++ src/data_loader.py
@@ -0,0 +1,27 @@
+import os
+import yfinance as yf
+import pandas as pd
+from typing import Optional
+
+def fetch_data(ticker: str, start_date: str, end_date: str, save_path: str ="data/raw") -> Optional[pd.DataFrame]:
+    os.makedirs(save_path, exist_ok=True)
+
+    try:
+        data = yf.download(ticker, start=start_date, end=end_date, auto_adjust=False)
+    except Exception as e:
+        print(f"Error downloading data for {ticker}: {e}")
+        return None
+
+    if data is None or data.empty:
+        print(f"No data for {ticker} between {start_date} and {end_date}.")
+        return None
+
+    file_name = f"{ticker}.csv"
+    file_path = os.path.join(save_path, file_name)
+    data.to_csv(file_path)
+
+    print(f"Data for {ticker} saved to {file_path}")
+    return data
+
+if __name__ == "__main__":
+    fetch_data("AAPL", "2020-01-01", "2021-01-01")
